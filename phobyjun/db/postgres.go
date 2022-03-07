cb9f505940810466cca00c87c48f5e3e01be26b2
phobyjun <phobyjun@gmail.com>
2022-03-08 02:05:37 +0900

    ADD: database connection

--- phobyjun/db/postgres.go
+++ phobyjun/db/postgres.go
@@ -3,24 +3,26 @@ package db
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"log"
 	"phobyjun/model"
 )
 
 const dsn = "host=localhost user=phobyjun password=password dbname=goropbox port=5432 TimeZone=Asia/Seoul"
 
-var db *gorm.DB
+var Session *gorm.DB
+var err error
 
 func Init() {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	Session, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic("DB Connection Error")
+		log.Panic("DB Connection Failed")
 	}
-	db.AutoMigrate(
+
+	err = Session.AutoMigrate(
 		&model.User{},
 		&model.File{},
 	)
-}
-
-func DBManager() *gorm.DB {
-	return db
+	if err != nil {
+		log.Panic("DB Migration Failed")
+	}
 }
--- phobyjun/main.go
+++ phobyjun/main.go
@@ -2,12 +2,36 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"net/http"
+	"phobyjun/db"
+	"phobyjun/model"
 )
 
 func main() {
+	db.Init()
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello Echo!")
 	})
+	e.GET("/api/v2/users", getUsers)
+	e.POST("/api/v2/users", createUsers)
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getUsers(c echo.Context) error {
+	tx := db.Session
+	var users []model.User
+	tx.Find(&users)
+	return c.JSON(http.StatusOK, users)
+}
+
+func createUsers(c echo.Context) error {
+	tx := db.Session
+	user := model.User{
+		Username: "junseok",
+		Password: "0000",
+		Email:    "phobyjun@gmail.com",
+	}
+	tx.Create(&user)
+	return c.JSON(http.StatusOK, user)
 }
