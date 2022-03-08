375c7dab55116f03d12c65580bd85c2130b3b489
phobyjun <phobyjun@gmail.com>
2022-03-08 20:09:27 +0900

    ADD: 파일 모델 생성

--- phobyjun/model/file.go
+++ phobyjun/model/file.go
@@ -1,4 +1,11 @@
 package model
 
+import "time"
+
 type File struct {
+	ID        uint      `gorm:"primary_key" json:"id"`
+	FileName  string    `json:"fileName"`
+	FileDir   string    `json:"fileDir"`
+	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"UpdatedAt"`
 }
