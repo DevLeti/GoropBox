77f9cc9134df6e174710a6ad51a1ce031de5bec7
phobyjun <phobyjun@gmail.com>
2022-03-11 11:11:15 +0900

    FIX: 불필요 함수 제거

--- phobyjun/main.go
+++ phobyjun/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"net/http"
 	"phobyjun/db"
-	"phobyjun/model"
 )
 
 func main() {
@@ -13,25 +11,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(200, "Hello Echo!")
 	})
-	e.GET("/api/v2/users", getUsers)
-	e.POST("/api/v2/users", createUsers)
 	e.Logger.Fatal(e.Start(":8080"))
-}
-
-func getUsers(c echo.Context) error {
-	tx := db.Session
-	var users []model.User
-	tx.Find(&users)
-	return c.JSON(http.StatusOK, users)
-}
-
-func createUsers(c echo.Context) error {
-	tx := db.Session
-	user := model.User{
-		Username: "junseok",
-		Password: "0000",
-		Email:    "phobyjun@gmail.com",
-	}
-	tx.Create(&user)
-	return c.JSON(http.StatusOK, user)
 }
