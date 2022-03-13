91a5badc858aa3249b21b65f4e86ebcc7d67cd5a
DevLeti <devleti0106@gmail.com>
2022-03-13 20:47:18 +0900

    EDIT: add dependency

--- devleti/main.go
+++ devleti/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	dsn := "host=localhost user=leti password=password dbname=goropbox port=5432 TimeZone=Asia/Seoul"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic("Failed to connect DB")
+	}
+
+	fmt.Println(db)
 }
